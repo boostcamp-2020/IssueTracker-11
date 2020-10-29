ed8bba42dfab0a64366302e5163d0b03101761d1
kakasoo <55487286+kakamura@users.noreply.github.com>
2020-10-29 13:13:45 +0900

    [WEB] chore: WEB 클라이언트 폴더 구조 #1
    - react atomic pattern 기반의 폴더 구조 설계

---
+++ web/client/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+    return (
+        <div className='App'>
+            <h1>hello react app!</h1>
+        </div>
+    );
+}
---
+++ web/client/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// TODO : server에서는 app.js인데 client에서는 App.js네요?
+import App from './App';
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    rootElement
+);
