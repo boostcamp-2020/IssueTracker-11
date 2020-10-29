dc9ce2ae9bb00ed1400660c49e6cc548ce53696f
Jay-Jay-Cho <whalswo7777@gmail.com>
2020-10-29 16:54:35 +0900

    [WEB] chore: webpack 환경설정 파일 추가 #4
    - entry, alias, module, plugins, dev-server 관련 설정 작성

---
+++ web/client/webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: ['@babel/polyfill', './src/index.js'],
+    output: {
+        path: path.resolve('./dist'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        alias: {
+            '@atoms': path.resolve(__dirname, 'src/components/atoms'),
+            '@molecules': path.resolve(__dirname, 'src/components/molecules'),
+            '@organisms': path.resolve(__dirname, 'src/components/organisms'),
+            '@pages': path.resolve(__dirname, 'src/components/pages'),
+            '@templates': path.resolve(__dirname, 'src/components/templates'),
+            '@themes': path.resolve(__dirname, 'src/components/themes'),
+        },
+    },
+    module: {
+        rules: [
+            { 
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|ico)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            useRelativePath: true,
+                            limit: 10000,
+                        },
+                    },
+                ],
+            },
+            { 
+                test: /\.html$/,
+                use: [
+                  {
+                    loader: 'html-loader',
+                    options: {
+                      minimize: true,
+                    },
+                  },
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+                exclude: /node_modules/,
+                use: ['file-loader?name=[name].[ext]'] 
+            }
+        ],
+    },
+    plugins: [new HtmlWebpackPlugin({ template: './public/index.html' }), new CleanWebpackPlugin()],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 3000,
+        compress: true,
+    },
+    // devtool: 'inline-source-map',
+};
