c2f75f65e2da49a90f954d2a33c213f8a1170977
kakasoo <55487286+kakamura@users.noreply.github.com>
2020-11-01 14:34:47 +0900

    [WEB] feat: 레이블 모델 컨트롤러로부터 분리 #21

--- web/server/controllers/labels.js
+++ web/server/controllers/labels.js
@@ -1,44 +1,28 @@
-import pool from '../config/database.js';
+import labelModel from '../models/labels.js';
 
-const GET_QUERY = `SELECT * FROM labels`;
-const POST_QUERY = 'INSERT INTO labels SET ?';
-const PUT_QUERY = ``;
-const DELETE_QUERY = ``;
-
-// TODO : set right status code!
+const DESCRIPTION_LIMIT = 128;
 
 class labelController {
     static get = async (req, res) => {
-        const connection = await pool.getConnection(async (connection) => connection);
         try {
-            const [result] = await connection.query(GET_QUERY);
-            return res.status(200).send(result);
+            const [result] = await labelModel.get();
+            return result.length === 0 ? res.status(204).send('No Content') : res.status(200).send(result);
         } catch (error) {
-            console.error(error);
-        } finally {
-            connection.release();
+            res.status(500).send({ result: error.message });
         }
     };
 
     static post = async (req, res) => {
         const { name, description, color } = req.body;
-        // TODO : title value check!
+        if (!name || (!!description && description.length > DESCRIPTION_LIMIT)) {
+            return res.status(422).send('Unprocessable Entity');
+        }
 
-        const connection = await pool.getConnection(async (connection) => connection);
         try {
-            await connection.beginTransaction();
-            const results = await connection.query(POST_QUERY, {
-                name: name,
-                color: color,
-                description: description,
-            });
-            await connection.commit();
-            // TODO : results's status check!
-            res.status(200).send(results);
+            const results = await labelModel.post(name, description, color);
+            return !results ? res.status(202).send('Accepted') : res.status(201).send('Created');
         } catch (error) {
-            console.error(error);
-        } finally {
-            connection.release();
+            res.status(500).send({ result: error.message });
         }
     };
 
