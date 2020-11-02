f73baa988704a999aac376539f981d6e6dc0eeb1
kgs19970120 <rmstn9007@naver.com>
2020-11-02 15:43:53 +0900

    [iOS] feat: Milestone 모델 초기 구현 #15
    - Milestone을 표현하기 위한 모델 초기 구성 완료

---
+++ iOS/IssueTracker/IssueTracker/Model/Milestone.swift
@@ -0,0 +1,17 @@
+//
+//  Milestone.swift
+//  IssueTracker
+//
+//  Created by 김근수 on 2020/11/02.
+//  Copyright © 2020 김근수. All rights reserved.
+//
+
+import Foundation
+
+struct Milestone {
+    var title: String
+    var dueDate: String?
+    var description: String
+    var openNumber: Int
+    var closedNumber: Int
+}
