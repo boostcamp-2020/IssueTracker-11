4f23885878d89fe8646631a1c9b7019f50f141db
kgs19970120 <rmstn9007@naver.com>
2020-11-02 15:40:23 +0900

    [iOS] feat: Milestone CollectionView Cell 구성 밑 연결
    - 마일스톤 정보를 표시하기 위한 Cell 구성 완료
    - 각 label IBoutlet으로 연결 완료

--- iOS/IssueTracker/IssueTracker/Milestone/MilestoneCollectionViewCell.swift
+++ iOS/IssueTracker/IssueTracker/Milestone/MilestoneCollectionViewCell.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
-class MilestoneCell: UICollectionViewCell {
-    
+class MilestoneCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var titleLabel: BorderLabel!
+    @IBOutlet weak var dueDateLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var openLabel: UILabel!
+    @IBOutlet weak var closedLabel: UILabel!
+    @IBOutlet weak var percentLabel: UILabel!
 }
