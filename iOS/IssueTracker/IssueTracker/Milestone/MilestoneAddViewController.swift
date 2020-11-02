8879b4018962922cd14497ac4e1442f144fca183
kgs19970120 <rmstn9007@naver.com>
2020-11-02 21:15:29 +0900

    [iOS] feat: 생성뷰 바깥 영역 터치시 dismiss 구현 #18
    - 생성뷰 바깥 영역 터치시 생성화면 빠져나가도록 구성 완료

--- iOS/IssueTracker/IssueTracker/Milestone/MilestoneAddViewController.swift
+++ iOS/IssueTracker/IssueTracker/Milestone/MilestoneAddViewController.swift
@@ -10,28 +10,83 @@ import UIKit
 
 class MilestoneAddViewController: UIViewController {
     
+    // MARK: - Properties
+    
     let creationFormView = CreationFormView()
+    let backgroundView = UIView()
 
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        configure()
+        creationFormView.delegate = self
+        configureBackgroundView()
+        configureCreationFormViewLayout()
+        configureGestureRecognizer()
     }
     
-    private func configure() {
+    override func viewWillDisappear(_ animated: Bool) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: - Methods
+    
+    private func configureCreationFormViewLayout() {
         self.view.addSubview(creationFormView)
         self.view.backgroundColor = UIColor(white: 0, alpha: 0.3)
         creationFormView.creationFormType = .milestone
         creationFormView.configureViewStyle()
-        configureConstraints()
+        configureCreationFormViewConstraints()
+    }
+    
+    private func configureCreationFormViewConstraints() {
+        creationFormView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            creationFormView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            creationFormView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
+            creationFormView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16)
+        ])
+    }
+    
+    private func configureBackgroundView() {
+        self.view.addSubview(backgroundView)
+        backgroundView.backgroundColor = .clear
+        configureBackGroundViewConstraints()
+    }
+    
+    private func configureBackGroundViewConstraints() {
+        backgroundView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            backgroundView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0),
+            backgroundView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0),
+            backgroundView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0),
+            backgroundView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0)
+        ])
+    }
+    
+    private func configureGestureRecognizer() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissView))
+        backgroundView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc private func dismissView() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+extension MilestoneAddViewController: CreationFormViewDelegate {
+    
+    func cancelButtonDidTap() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func resetButtonDidTap() {
+        creationFormView.inputTextFields.forEach { $0.text = "" }
+    }
+    
+    func saveButtonDidTap() {
         
     }
     
-    private func configureConstraints() {
-        creationFormView.translatesAutoresizingMaskIntoConstraints = false
-      NSLayoutConstraint.activate([
-        creationFormView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
-        creationFormView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
-        creationFormView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16)
-      ])
-    }
 }
