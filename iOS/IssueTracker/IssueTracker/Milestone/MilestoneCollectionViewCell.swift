47fe4eb96bbb2bd505ad7b4c9fecad993f9a4d48
kgs19970120 <rmstn9007@naver.com>
2020-11-02 15:38:32 +0900

    [iOS] feat: Border label 구현 #15
    - 마일스톤 타이틀을 표시하기 위해 border가 있는 Label 작성 완료

---
+++ iOS/IssueTracker/IssueTracker/Milestone/MilestoneCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MilestoneCell.swift
+//  IssueTracker
+//
+//  Created by 김근수 on 2020/11/02.
+//  Copyright © 2020 김근수. All rights reserved.
+//
+
+import UIKit
+
+class MilestoneCell: UICollectionViewCell {
+    
+}
