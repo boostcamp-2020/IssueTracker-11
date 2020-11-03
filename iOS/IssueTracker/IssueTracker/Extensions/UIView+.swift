8f2597cf7b69d092e8afa66d852425a9dcc6ebc8
kgs19970120 <rmstn9007@naver.com>
2020-11-03 19:46:09 +0900

    [iOS] style: SwipeableCollectionCell 에 있던 UIView extension 분리 완료 #31

--- iOS/IssueTracker/IssueTracker/Extensions/UIView+.swift
+++ iOS/IssueTracker/IssueTracker/Extensions/UIView+.swift
@@ -6,4 +6,17 @@
 //  Copyright © 2020 김근수. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    
+    func pinEdgesToSuperView() {
+        guard let superView = superview else { return }
+        translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: superView.topAnchor).isActive = true
+        leftAnchor.constraint(equalTo: superView.leftAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: superView.bottomAnchor).isActive = true
+        rightAnchor.constraint(equalTo: superView.rightAnchor).isActive = true
+    }
+    
+}
