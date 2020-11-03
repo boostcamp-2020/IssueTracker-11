f9dfd01165dd0f81124fcba08e7276052c996992
Jay-Jay-Cho <whalswo7777@gmail.com>
2020-11-03 17:43:15 +0900

    [WEB] chore: 개발/배포 분기 처리 추가 #4
    - package.json으로 부터 넘어오는 mode값에 따라 webpack bundle.js 분기 생성

--- web/client/webpack.config.js
+++ web/client/webpack.config.js
@@ -1,68 +1,99 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
-module.exports = {
-    mode: 'development',
-    entry: ['@babel/polyfill', './src/index.js'],
-    output: {
-        path: path.resolve('./dist'),
-        filename: 'bundle.js',
-    },
-    resolve: {
-        alias: {
-            '@atoms': path.resolve(__dirname, 'src/components/atoms'),
-            '@molecules': path.resolve(__dirname, 'src/components/molecules'),
-            '@organisms': path.resolve(__dirname, 'src/components/organisms'),
-            '@pages': path.resolve(__dirname, 'src/components/pages'),
-            '@templates': path.resolve(__dirname, 'src/components/templates'),
-            '@themes': path.resolve(__dirname, 'src/components/themes'),
+module.exports = (env, options) => {
+    const config = {
+        entry: ['@babel/polyfill', './src/index.js'],
+        output: {
+            filename: '[name].bundle.js',
+            path: path.resolve(__dirname, 'dist'),
         },
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
+        optimization: {
+            splitChunks: {
+                cacheGroups: {
+                    commons: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: 'vendors',
+                        chunks: 'all',
+                    },
                 },
             },
-            {
-                test: /\.(png|jpe?g|gif|svg|ico)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            useRelativePath: true,
-                            limit: 10000,
-                        },
+        },
+        resolve: {
+            alias: {
+                '@atoms': path.resolve(__dirname, 'src/components/atoms'),
+                '@molecules': path.resolve(__dirname, 'src/components/molecules'),
+                '@organisms': path.resolve(__dirname, 'src/components/organisms'),
+                '@pages': path.resolve(__dirname, 'src/components/pages'),
+                '@templates': path.resolve(__dirname, 'src/components/templates'),
+                '@themes': path.resolve(__dirname, 'src/components/themes'),
+            },
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
                     },
-                ],
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: 'html-loader',
-                        options: {
-                            minimize: true,
+                },
+                {
+                    test: /\.(png|jpe?g|gif|svg|ico)$/,
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                useRelativePath: true,
+                                limit: 10000,
+                            },
                         },
-                    },
-                ],
+                    ],
+                },
+                {
+                    test: /\.html$/,
+                    use: [
+                        {
+                            loader: 'html-loader',
+                            options: {
+                                minimize: true,
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+                    exclude: /node_modules/,
+                    use: ['file-loader?name=[name].[ext]'],
+                },
+            ],
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new webpack.HotModuleReplacementPlugin(),
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, "./public/index.html"),
+                showErrors: true,
+                filename: path.join(__dirname, './dist/index.html')
+            }),
+        ]
+    };
+
+    if (options.mode === 'development') {
+        config.devtool = 'inline-source-map';
+        config.devServer = {
+            hot: true,
+            host: '0.0.0.0',
+            port: 3000,
+            contentBase: path.resolve(__dirname, 'dist'),
+            compress: true,
+            stats: {
+                color: true,
             },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
-                exclude: /node_modules/,
-                use: ['file-loader?name=[name].[ext]'],
-            },
-        ],
-    },
-    plugins: [new HtmlWebpackPlugin({ template: './public/index.html' }), new CleanWebpackPlugin()],
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        port: 3000,
-        compress: true,
-    },
-    // devtool: 'inline-source-map',
+        };
+    }
+
+    return config;
 };
