32903c3f09c728b7be96fe5f33c7689d17e80e07
eojin <eozinee@gmail.com>
2020-11-03 13:11:50 +0900

    [iOS] docs: 파일 구조 변경

--- iOS/IssueTracker/IssueTracker/Extensions/UIColor+.swift
+++ iOS/IssueTracker/IssueTracker/Extensions/UIColor+.swift
@@ -34,7 +34,7 @@ extension UIColor {
         var blue: CGFloat = 0
         var alpha: CGFloat = 0
         getRed(&red, green: &green, blue: &blue, alpha: &alpha)
-        let rgb: Int = (Int)(red*255)<<16 | (Int)(green*255)<<8 | (Int)(blue*255)<<0
+        let rgb: Int = (Int)(red * 255) << 16 | (Int)(green * 255) << 8 | (Int)(blue * 255) << 0
         
         return NSString(format: "#%06x", rgb).uppercased as String
     }
