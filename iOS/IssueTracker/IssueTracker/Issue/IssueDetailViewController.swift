2effc89526f4828eb6d4b0d7365d45acbc9629b5
eojin <eozinee@gmail.com>
2020-11-04 19:58:43 +0900

    [iOS] feat: 이슈 상세 뷰 컨트롤러 구현 #35 - header, cell 라벨 길이에 따른 사이징

--- iOS/IssueTracker/IssueTracker/Issue/IssueDetailViewController.swift
+++ iOS/IssueTracker/IssueTracker/Issue/IssueDetailViewController.swift
@@ -8,23 +8,81 @@
 
 import UIKit
 
-class IssueDetailViewController: UIViewController {
-
+final class IssueDetailViewController: UIViewController {
+    
+    @IBOutlet private weak var commentCollectionView: UICollectionView!
+    
+    private var commentList: [String]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        loadComment()
+        setFlowLayout()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func loadComment() {
+        commentList = [ "ㅋㅋㅋㅋㅋ", "댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글댓글", "잘하네요"]
     }
-    */
+    
+    private func setFlowLayout() {
+        guard let flowLayout = commentCollectionView.collectionViewLayout as? UICollectionViewFlowLayout else { return }
+        flowLayout.estimatedItemSize = CGSize(width: commentCollectionView.frame.width, height: 200)
+    }
+    
+}
 
+extension IssueDetailViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView,
+                        viewForSupplementaryElementOfKind kind: String,
+                        at indexPath: IndexPath) -> UICollectionReusableView {
+        guard let headerView = collectionView
+            .dequeueReusableSupplementaryView(ofKind: kind,
+                                              withReuseIdentifier: "CommentHeaderReusableView",
+                                              for: indexPath) as? CommentHeaderReusableView
+            else { return UICollectionReusableView() }
+        
+        return headerView
+    }
+}
+
+extension IssueDetailViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return commentList?.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView
+            .dequeueReusableCell(withReuseIdentifier: "CommentCollectionViewCell",
+                                 for: indexPath) as? CommentCollectionViewCell,
+            let comment = commentList?[indexPath.item]
+            else { return UICollectionViewCell() }
+        
+        cell.conigure(text: comment)
+        
+        return cell
+    }
+    
+}
+
+// MARK: - CollectionViewDelegateFlowLayout
+extension IssueDetailViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        referenceSizeForHeaderInSection section: Int) -> CGSize {
+        
+        let indexPath = IndexPath(row: 0, section: section)
+        let headerView = self.collectionView(collectionView,
+                                             viewForSupplementaryElementOfKind: UICollectionView.elementKindSectionHeader,
+                                             at: indexPath)
+        
+        return headerView.systemLayoutSizeFitting(CGSize(width: collectionView.frame.width,
+                                                         height: UIView.layoutFittingExpandedSize.height),
+                                                  withHorizontalFittingPriority: .required,
+                                                  verticalFittingPriority: .fittingSizeLevel)
+    }
+    
 }
