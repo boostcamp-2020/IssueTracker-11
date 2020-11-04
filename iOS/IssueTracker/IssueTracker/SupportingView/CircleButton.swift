4898ff76b9c6655fba00812ac7b5f7a8ba0d4891
kgs19970120 <rmstn9007@naver.com>
2020-11-04 14:33:45 +0900

    [iOS] feat: CircleButton 작성
    - 둥근 버튼 재사용을 위한 CircleButton class 작성

--- iOS/IssueTracker/IssueTracker/SupportingView/CircleButton.swift
+++ iOS/IssueTracker/IssueTracker/SupportingView/CircleButton.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2020 김근수. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+@IBDesignable
+class CircleButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        configure()
+    }
+    
+    private func configure() {
+        let radius = bounds.width / 2
+        layer.cornerRadius = radius
+        layer.masksToBounds = true
+        contentEdgeInsets = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+    }
+    
+}
