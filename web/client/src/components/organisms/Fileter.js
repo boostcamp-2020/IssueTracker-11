4ea0a2ef986380b941d98736a1760dc23b162183
Jay-Jay-Cho <whalswo7777@gmail.com>
2020-11-05 14:17:51 +0900

    [WEB] feat: 이슈리스트 필터적용 컴포넌트 스켈레톤 코드 작성 #50
    - 이슈리스트를 필터링할 수 있는 컴포넌트의 스켈레톤 코드 작성

---
+++ web/client/src/components/organisms/Fileter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CheckBox from '@atoms/CheckBox';
+
+export default function Filter() {
+    return (
+        <div>
+            <CheckBox />;
+        </div>
+    );
+}
