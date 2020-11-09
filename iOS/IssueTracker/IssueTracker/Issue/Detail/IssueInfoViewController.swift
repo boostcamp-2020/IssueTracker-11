63803e9782437981a78b8286e25bba9656e6370e
kgs19970120 <rmstn9007@naver.com>
2020-11-09 22:19:10 +0900

    [iOS] feat: PullUpView 생성 #39

---
+++ iOS/IssueTracker/IssueTracker/Issue/Detail/IssueInfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  IssueInfoViewController.swift
+//  IssueTracker
+//
+//  Created by 김근수 on 2020/11/05.
+//  Copyright © 2020 김근수. All rights reserved.
+//
+
+import UIKit
+
+class IssueInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iOS/IssueTracker/IssueTracker/Issue/Detail/ProfileView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileView.swift
+//  IssueTracker
+//
+//  Created by 김근수 on 2020/11/09.
+//  Copyright © 2020 김근수. All rights reserved.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
