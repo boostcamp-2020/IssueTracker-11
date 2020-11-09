45db62a52decdaef52867b59d8c7d884ce2d6507
kakasoo <55487286+kakamura@users.noreply.github.com>
2020-11-09 19:14:42 +0900

    [WEB] feat: page 전환 기능 구현 #50

---
+++ web/client/src/components/pages/Label.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledLabel = styled.div``;
+
+const Label = () => {
+    return <div>Hello Labels</div>;
+};
+
+export default Label;
---
+++ web/client/src/components/pages/Milestone.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Milestone = () => {
+    return <div>HELLO MILESTONES</div>;
+};
+
+export default Milestone;
