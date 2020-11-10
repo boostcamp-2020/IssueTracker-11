39af356c1c4d12916a01134045f02c730c48db91
kgs19970120 <rmstn9007@naver.com>
2020-11-10 20:10:38 +0900

    [iOS] Apple Login Button layout 구성 및 IBOulets 연결 #68

--- iOS/IssueTracker/IssueTracker/Login/LoginViewController.swift
+++ iOS/IssueTracker/IssueTracker/Login/LoginViewController.swift
@@ -7,24 +7,64 @@
 //
 
 import UIKit
+import AuthenticationServices
 
 class LoginViewController: UIViewController {
 
+    // MARK: - IBOutlet
+    
+    @IBOutlet weak var githubLoginButton: UIButton!
+    @IBOutlet weak var appleLoginButtonContainer: UIView!
+    
+    // MARK: - Properties
+    
+    private var appleLoginButton = ASAuthorizationAppleIDButton(type: .signIn, style: .black)
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        congiureAppleLoginButton()
+        registerButtonActions()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Methods
+    
+    private func congiureAppleLoginButton() {
+        appleLoginButtonContainer.addSubview(appleLoginButton)
+        appleLoginButton.pinEdgesToSuperView()
     }
-    */
+    
+    private func registerButtonActions() {
+        appleLoginButton.addTarget(self,
+                                   action: #selector(appleLogin),
+                                   for: .touchUpInside)
+    }
+    
+    // MARK: - Objc
+    
+    @objc fileprivate func appleLogin() {
+            
+        let appleIdRequest = ASAuthorizationAppleIDProvider().createRequest()
+        appleIdRequest.requestedScopes = [.email, .fullName]
 
+        let controller = ASAuthorizationController(authorizationRequests: [appleIdRequest])
+        controller.delegate = self
+        controller.presentationContextProvider = self
+        controller.performRequests()
+    }
+
+}
+
+extension LoginViewController: ASAuthorizationControllerDelegate {
+    
+}
+
+extension LoginViewController: ASAuthorizationControllerPresentationContextProviding {
+    
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        guard let window = self.view.window else { return UIWindow() }
+        return window
+    }
+    
 }
