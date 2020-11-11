994d74f05a09da8f6b3fe4ae136c64b48bd3cf56
Jay-Jay-Cho <whalswo7777@gmail.com>
2020-11-11 19:58:21 +0900

    [WEB] fix: Label Dropdown 아이템 좌측에 Circle Component 추가

--- web/client/src/components/atoms/ListItem.js
+++ web/client/src/components/atoms/ListItem.js
@@ -1,12 +1,15 @@
 import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { DropDownContext } from '@organisms/DropDown.js';
+import Circle from '@atoms/Circle';
 
 const StyledListItem = styled.li`
     border-top: 1px solid gray;
     padding: 5px 30px;
     width: 100%;
     list-style: none;
+    display: flex;
+    align-items: center;
 `;
 
 const ListItem = ({ item }) => {
@@ -14,7 +17,13 @@ const ListItem = ({ item }) => {
     const onClickMethod = () => {
         onSelect(item);
     };
-    return <StyledListItem onClick={onClickMethod}>{item.title || item.name || item.email}</StyledListItem>;
+    console.log(item);
+    return (
+        <StyledListItem onClick={onClickMethod}>
+            {item.name ? <Circle height="10px" color={item.color} /> : null}
+            {item.title || item.name || item.email}
+        </StyledListItem>
+    );
 };
 
 export default ListItem;
