4b492b021c9f279880a7da8c30bf1ff0e6fbfda8
shany <shany7540@naver.com>
2020-11-12 18:25:40 +0900

    [WEB] feat: 이슈 상태 일괄 변경 fetch 함수 작성 #64
    - Markas 드롭다운에 items 적용 시킨 후 클릭시 이슈 상태 변경 api 요청함수 작성

--- web/client/src/components/atoms/ListItem.js
+++ web/client/src/components/atoms/ListItem.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { DropDownContext } from '@organisms/DropDown.js';
 import Circle from '@atoms/Circle';
 import { AppContext } from '../../App';
+import { CheckboxContext } from '../../store/CheckboxStore';
 
 const StyledListItem = styled.li`
     border-top: 1px solid gray;
@@ -15,11 +16,28 @@ const StyledListItem = styled.li`
 
 const ListItem = ({ item }) => {
     const { onSelectData } = useContext(AppContext);
-    const onClickMethod = () => onSelectData(item);
+    const { checkState } = useContext(CheckboxContext);
+    const markAsEvent = (item) => {
+        let status = undefined;
+        if (item === 'open') status = 1;
+        else status = 0;
+
+        const changeStatus = async () => {
+            const res = await fetch(`http://49.50.160.103:3000/issues/${status}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(checkState.ids),
+            });
+        };
+        changeStatus();
+    };
+    const onClickMethod = item === 'open' || item === 'closed' ? () => markAsEvent(item) : () => onSelectData(item);
     return (
         <StyledListItem onClick={onClickMethod}>
             {item.name ? <Circle height="10px" color={item.color} /> : null}
-            {item.title || item.name || item.email}
+            {item.title || item.name || item.email || item}
         </StyledListItem>
     );
 };
