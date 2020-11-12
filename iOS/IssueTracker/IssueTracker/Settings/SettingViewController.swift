ca8373271f49f37277d5c46f82984f0632b02de1
kgs19970120 <rmstn9007@naver.com>
2020-11-12 14:14:25 +0900

    [iOS] feat: Apple Login 추가 및 구현

---
+++ iOS/IssueTracker/IssueTracker/Settings/SettingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingViewController.swift
+//  IssueTracker
+//
+//  Created by 김근수 on 2020/11/12.
+//  Copyright © 2020 김근수. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
