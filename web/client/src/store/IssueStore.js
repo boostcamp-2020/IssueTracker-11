182088d999eb987bb66e47c724f1c253f220130b
shany <shany7540@naver.com>
2020-11-12 12:29:34 +0900

    [WEB] feat: 이슈 store 생성 #64
    - 이슈상태를 관리하기 위한 store컴포넌트 생성

--- web/client/src/store/IssueStore.js
+++ web/client/src/store/IssueStore.js
@@ -1,18 +1,31 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect } from 'react';
 
 export const IssueContext = React.createContext();
-const issueReducer = (issues, { type }) => {
+const issueReducer = (issues, { type, data }) => {
     switch (type) {
-        case 'A':
+        case 'GET':
+            return data;
+        case 'CREATE':
             return;
-        case 'B':
+        case 'UPDATE':
             return;
         default:
-            return;
+            return issues;
     }
 };
 
 export const IssueStore = ({ children }) => {
-    const [issues, issueDispatch] = useReducer(issueReducer, []);
-    return <IssueContext.Provider value={{ issues, issueDispatch }}></IssueContext.Provider>;
+    const [issues, issueDispatch] = useReducer(issueReducer, {});
+    const setInitState = async () => {
+        const response = await fetch('http://49.50.160.103:3000/issues');
+        const data = await response.json();
+        console.log(data, ';;;;');
+        issueDispatch({ type: 'GET', data });
+    };
+    useEffect(() => {
+        setInitState();
+    }, []);
+    return <IssueContext.Provider value={{ issues, issueDispatch }}>{children}</IssueContext.Provider>;
 };
+
+// 필터클릭시 input에 넣기 -> ref 로 돔 직접 가져오면?
