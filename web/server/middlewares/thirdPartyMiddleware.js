1ba9c0c88bf415f1078de3e9af3498e7e1ba053b
kakasoo <55487286+kakamura@users.noreply.github.com>
2020-11-12 18:25:42 +0900

    [WEB] fix: cors 에러 서버측에서 해결 #44

--- web/server/middlewares/thirdPartyMiddleware.js
+++ web/server/middlewares/thirdPartyMiddleware.js
@@ -2,6 +2,11 @@ import compression from 'compression';
 import helmet from 'helmet';
 import cors from 'cors';
 
-const thirdPartyMiddleware = [compression(), helmet(), cors()];
+const corsOption = {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+};
+
+const thirdPartyMiddleware = [compression(), helmet(), cors(corsOption)];
 
 export default thirdPartyMiddleware;
